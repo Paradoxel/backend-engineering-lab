953014e56b3b36169087d8eb11de51e1b3a95547
Mohammadreza <a1mmdrez@gmail.com>
2026-06-19 18:37:08 +0330

    feat: build JWT header and finalize token structure with signature protection

---
+++ experiments/jwt_experiment.py
@@ -0,0 +1,82 @@
+import json
+import base64
+import hmac
+import hashlib
+
+
+
+# Fake user database
+users = {
+    "Mohmmadreza": {
+        "id": 1,
+        "password": "1234"
+    }
+}
+
+
+
+# Secret key (server-side only)
+SECRET_KEY = "supersecret"
+
+
+
+# Helper: Base64 encoding
+def base64_encode(data: str) -> str:
+    return base64.b64encode(data.encode()).decode()
+
+
+
+# Login function (JWT creation)
+def login(username, password):
+    user = users.get(username)
+
+    # user validation
+    if not user:
+        return None
+
+    if password != user["password"]:
+        return None
+
+    
+    # JWT HEADER 
+    header = {
+        "alg": "HS256",
+        "typ": "JWT"
+    }
+
+    
+    # JWT PAYLOAD 
+    payload = {
+        "user_id": user["id"],
+        "role": "admin"
+    }
+
+    
+    # Convert to JSON
+    header_json = json.dumps(header)
+    payload_json = json.dumps(payload)
+
+    
+    # Base64 encoding
+    header_base64 = base64_encode(header_json)
+    payload_base64 = base64_encode(payload_json)
+
+    
+    # SIGNATURE (HMAC SHA256)
+    
+    signature = hmac.new(
+        SECRET_KEY.encode(),
+        payload_base64.encode(),
+        hashlib.sha256
+    ).hexdigest()
+
+    
+    # FINAL JWT STRUCTURE (almost real)
+    jwt_token = f"{header_base64}.{payload_base64}.{signature}"
+
+    return jwt_token
+
+
+
+# Test
+print(login("Mohmmadreza", "1234"))
