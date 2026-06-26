c4973c4ef1734971a9dbbb44fb785d556eb37125
Mohammadreza <a1mmdrez@gmail.com>
2026-06-26 23:57:55 +0330

    feat(shop): add Category and Product models with ForeignKey relationship

--- practice/django_lab/shop/models.py
+++ practice/django_lab/shop/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.db import models 
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,related_name='products')
+
+    def __str__(self):
+        return self.name
