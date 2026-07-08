957681e3397f78fdc642ff112ead7ed03900ca4c
Mohammadreza <a1mmdrez@gmail.com>
2026-07-08 22:12:32 +0330

    feat(task-api): add GET /tasks endpoint

--- docker/task-api/app.py
+++ docker/task-api/app.py
@@ -2,6 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+tasks = [
+    {
+        "id": 1,
+        "title": "Learn Docker",
+        "completed": False,
+    },
+    {
+        "id": 2,
+        "title": "Build Flask API",
+        "completed": True,
+    },
+]
+
+
 
 @app.route("/")
 def home():
@@ -9,6 +23,11 @@ def home():
         "message": "Welcome to Task API",
         "status": "running",
     }
+
+
+@app.route("/tasks")
+def get_tasks():
+    return tasks
 
 
 if __name__ == "__main__":
