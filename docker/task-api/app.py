bde025a4a90338ae4825f73506dbac5c9fc0a3a9
Mohammadreza <a1mmdrez@gmail.com>
2026-07-08 22:33:24 +0330

    feat(task-api): add POST /tasks endpoint

--- docker/task-api/app.py
+++ docker/task-api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -41,6 +41,20 @@ def get_task(task_id):
             "error": "Task not found"
         }
     ), 404
+
+
+@app.route("/tasks",method=["POST"])
+def create_task():
+    data = request.get_json()
+
+    task ={
+        "id":len(tasks)+1,
+        "title":data['title'],
+        "completed":False
+    }
+
+    tasks.append(task)
+    return jsonify(task),201
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
