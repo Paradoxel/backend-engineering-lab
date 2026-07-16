30f86af1cb5b0451ee86e6091e9bb9a0a0e1c68c
Mohammadreza <a1mmdrez@gmail.com>
2026-07-17 02:44:36 +0330

    feat(accounts): add user manager for custom user

--- django-advanced-lab/accounts/models.py
+++ django-advanced-lab/accounts/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-
+from accounts.managers import UserManager
 
 class User(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(
         unique=True,
-        max_length=255
+        max_length=255,
+        db_index=True
     )
 
     first_name = models.CharField(
@@ -33,6 +34,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = "email"
 
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
--- django-advanced-lab/config/settings.py
+++ django-advanced-lab/config/settings.py
@@ -125,3 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
+
+
+
+# Custom User
+AUTH_USER_MODEL='accounts.User'
