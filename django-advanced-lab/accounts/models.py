06dfe84343938c18a68d449a99e9807cca4133fb
Mohammadreza <a1mmdrez@gmail.com>
2026-07-16 18:52:32 +0330

    feat(accounts): add custom user model

--- django-advanced-lab/accounts/models.py
+++ django-advanced-lab/accounts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(
+        unique=True,
+        max_length=255
+    )
+
+    first_name = models.CharField(
+        max_length=255,
+        blank=True
+    )
+
+    is_staff = models.BooleanField(
+        default=False
+    )
+
+    is_active = models.BooleanField(
+        default=True
+    )
+
+    created_date = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_date = models.DateTimeField(
+        auto_now=True
+    )
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
