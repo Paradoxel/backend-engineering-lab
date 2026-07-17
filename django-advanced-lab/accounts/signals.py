89682f6afa2fa9d60b024587fc1f0af30e76dec4
Mohammadreza <a1mmdrez@gmail.com>
2026-07-18 02:53:04 +0330

    refactor: move django advanced project to a dedicated repository

--- django-advanced-lab/accounts/signals.py
+++
@@ -1,7 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from accounts.models import User,Profile
-@receiver(post_save, sender=User)
-def create_profile_after_user(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
