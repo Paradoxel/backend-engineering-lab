5958f5cb06e781bc5e21068276c7bec1e05692e4
Mohammadreza <a1mmdrez@gmail.com>
2026-07-17 17:19:53 +0430

    feat(accounts): configure custom user admin interface

--- django-advanced-lab/accounts/admin.py
+++ django-advanced-lab/accounts/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    ordering = ("-created_date",)
+    list_display = (
+        "email",
+        "first_name",
+        "is_staff",
+        "is_active",
+    )
+    search_fields = ("email", "first_name")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name",)}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
